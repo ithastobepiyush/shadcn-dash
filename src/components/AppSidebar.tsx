e179b2d8fee64b946fd88320e57f81b1f590035f
Piyush Tiwari <ithastobepiyush@gmail.com>
2026-03-06 13:23:34 +0530

    Sidebar Component Initiated/ Dark Mode Done/ Profile Done'

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import { Home, Inbox, Calendar, Search, Settings } from "lucide-react"
+import { Sidebar, SidebarHeader, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarGroupContent, SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "./ui/sidebar"
+import Link from "next/link"
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings
+  },
+];
 
 const AppSidebar = () => {
   return (
-    <div>AppSidebar</div>
+    <Sidebar>
+      <SidebarHeader></SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title} >
+                  <SidebarMenuButton>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,57 @@
+"use client"
+
 import Link from "next/link"
-import {LogOut, Moon, Settings, SquareMenu, User} from "lucide-react"
+import {LogOut, Moon, Sun,Settings, User} from "lucide-react"
 import Image from "next/image"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+import { useTheme } from "next-themes"
 
 
 const Navbar = () => {
-  return (
+    const {theme, setTheme} = useTheme();
+    return (
     <nav className="p-4 flex items-center justify-between">
         {/* Left SIde */}
         collapseButton
         {/* Right Side */}
         <div className="flex items-center gap-4">
             <Link href="/">Dashboard</Link>
-            <Moon />
-            
+
+            {/* Theme Menu */}
+
+             <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="icon">
+                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                    <span className="sr-only">Toggle theme</span>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                    Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                    Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                    System
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+                </DropdownMenu>
+
+
+            {/* User Menu */}
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Avatar>
